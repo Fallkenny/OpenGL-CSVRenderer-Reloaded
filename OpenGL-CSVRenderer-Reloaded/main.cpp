52e754f76b9c689857221625e55db7e7a7b9eb4d
Will Mascarello <william.mascarello@duo.studio>
2020-12-02 22:58:17 -0300

    Sol parcial

--- OpenGL-CSVRenderer-Reloaded/main.cpp
+++ OpenGL-CSVRenderer-Reloaded/main.cpp
@@ -321,9 +321,9 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
-        lightPos.x = cos(glfwGetTime()) * 1.0f; // 0, 1, 0, 1
-        lightPos.y = sin(glfwGetTime()) * 5.0f; // 0, 1, 0, 1
-        lightPos.z = 1.0f + sin(glfwGetTime()) * 5.0f; // 0, 1, 0, 1
+        lightPos.x = cos(glfwGetTime()) * 1.0f;
+        lightPos.y = sin(glfwGetTime()) * 5.0f;
+        lightPos.z = cos(glfwGetTime()) * 5.0f;
 
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
