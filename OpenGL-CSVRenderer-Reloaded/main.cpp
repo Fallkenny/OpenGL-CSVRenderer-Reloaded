6c181c4f9268d7e34cae3b9f4d2cf13a845e0ca6
Felipe Fischer <ffsilva8@ucs.br>
2020-12-02 22:29:21 -0300

    loading multiple objs

--- OpenGL-CSVRenderer-Reloaded/main.cpp
+++ OpenGL-CSVRenderer-Reloaded/main.cpp
@@ -10,9 +10,9 @@
 #include "camera.h"
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
 // settings
@@ -35,17 +35,28 @@ glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 // Reflexo especular
 float specularStrength = 0.5;
 
-std::pair<std::string,std::vector<float>>  read_csv(std::string filename)
+typedef struct
+{
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int texture;
+    float *vertexes;
+    int pointsCount;
+
+} RenderableObj;
+
+std::pair<std::string, std::vector<float>> read_csv(std::string filename)
 {
     std::vector<float> resultVector;
     std::string texture;
     std::ifstream myFile(filename);
-    if(!myFile.is_open()) throw std::runtime_error("Could not open file");
+    if (!myFile.is_open())
+        throw std::runtime_error("Could not open file");
 
     std::string line, colname;
     float val;
 
-    if(myFile.good())
+    if (myFile.good())
     {
         std::stringstream ss(line);
     }
@@ -53,18 +64,19 @@ std::pair<std::string,std::vector<float>>  read_csv(std::string filename)
     std::stringstream ss(line);
     ss >> texture;
 
-
-    while(std::getline(myFile, line))
+    while (std::getline(myFile, line))
     {
         std::stringstream ss(line);
 
         int colIdx = 0;
 
-        while(ss >> val){
+        while (ss >> val)
+        {
 
             resultVector.push_back(val);
 
-            if(ss.peek() == ';') ss.ignore();
+            if (ss.peek() == ';')
+                ss.ignore();
 
             colIdx++;
         }
@@ -72,7 +84,89 @@ std::pair<std::string,std::vector<float>>  read_csv(std::string filename)
 
     myFile.close();
 
-    return std::make_pair(texture,  resultVector);
+    return std::make_pair(texture, resultVector);
+}
+
+RenderableObj load_renderableObj(std::string file)
+{
+    RenderableObj obj;
+
+    std::pair<std::string, std::vector<float>> content = read_csv(file);
+    std::string textureIMG = content.first;
+    std::vector<float> vertexes = content.second;
+
+    int vectorSize = vertexes.size();
+    float *vertices = new float[vectorSize];
+    for (size_t i = 0; i < vectorSize; i++)
+    {
+        vertices[i] = vertexes[i];
+    }
+    std::cout << vectorSize / 11 << std::endl;
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // first, configure the cube's VAO (and VBO)
+    unsigned int VBO, objVAO;
+    glGenVertexArrays(1, &objVAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(objVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vectorSize, vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    // normal attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //objColor attribute
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    //texture coord attribute
+    glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void *)(9 * sizeof(float)));
+    glEnableVertexAttribArray(3);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(1);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    std::string tmp = "textures/" + textureIMG;
+    unsigned char *data = stbi_load(tmp.c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        // Se a imagem for PNG com transparência
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        // Se a imagem for JPG, e portanto sem transparência
+        //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    obj.pointsCount = vectorSize / 11;
+    obj.vertexes = vertices;
+    obj.VAO = objVAO;
+    obj.VBO = VBO;
+    obj.texture = texture;
+
+    return obj;
 }
 
 int main()
@@ -90,7 +184,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Modelo de Iluminação Phong", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Modelo de Iluminação Phong", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -107,9 +201,12 @@ int main()
 
     // glew: load all OpenGL function pointers
     // ---------------------------------------
-    if(glewInit()!=GLEW_OK) {
+    if (glewInit() != GLEW_OK)
+    {
         std::cout << "Ocorreu um erro iniciando GLEW!" << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "GLEW OK!" << std::endl;
         std::cout << glGetString(GL_VERSION) << std::endl;
     }
@@ -131,124 +228,61 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float verticesCube[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+        0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+        0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
 
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
 
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
 
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
 
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
 
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f};
+
+    std::string models[] = 
+    {
+        "chao.csv",
+        "chao2.csv"
     };
 
-    std::pair<std::string,std::vector<float>> content =  read_csv("chao.csv");
-    std::string textureIMG = content.first;
-    std::vector<float> vertexes  = content.second;
+    int modelscount = sizeof(models)/sizeof(models[0]);
+    RenderableObj *objects = new RenderableObj[modelscount];
 
-    int vectorSize = vertexes.size();
-    float* vertices = new float[vectorSize];
-    for (size_t i = 0; i < vectorSize; i++) {
-        vertices[i] = vertexes[i];
-    }
-    std::cout << vectorSize/11 << std::endl;
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    // first, configure the cube's VAO (and VBO)
-    unsigned int VBO, objVAO;
-    glGenVertexArrays(1, &objVAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(objVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)* vectorSize, vertices, GL_STATIC_DRAW);
-
-
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // normal attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    //objColor attribute
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    //texture coord attribute
-    glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(9 * sizeof(float)));
-    glEnableVertexAttribArray(3);
-
-
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(1);
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-    std::string tmp = "textures/"+ textureIMG;
-    unsigned char *data = stbi_load(tmp.c_str(), &width, &height, &nrChannels, 0);
-    if (data)
+    for (int i = 0; i < modelscount; i++)
     {
-        // Se a imagem for PNG com transparência
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        // Se a imagem for JPG, e portanto sem transparência
-        //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-
-        glGenerateMipmap(GL_TEXTURE_2D);
+        objects[i] = load_renderableObj(models[i]);
     }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-
-
-
-
-
 
     // second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
     unsigned int lightCubeVAO, lightCubeVBO;
@@ -261,12 +295,11 @@ int main()
     glBindVertexArray(lightCubeVAO);
 
     // note that we update the lamp's position attribute's stride to reflect the updated buffer data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
-   // normal attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    // normal attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
 
     // render loop
     // -----------
@@ -288,10 +321,9 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
-        lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
-        lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
-
-        glBindTexture(GL_TEXTURE_2D, texture);
+        lightPos.x = cos(glfwGetTime()) * 0.5f; // 0, 1, 0, 1
+        lightPos.y = sin(glfwGetTime()) * 10.0f; // 0, 1, 0, 1
+        lightPos.z = sin(glfwGetTime()) * 10.0f; // 0, 1, 0, 1
 
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
@@ -299,7 +331,7 @@ int main()
         lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
         lightingShader.setVec3("lightPos", lightPos);
         lightingShader.setVec3("viewPos", camera.Position);
-        lightingShader.setFloat("specularStrength",specularStrength);
+        lightingShader.setFloat("specularStrength", specularStrength);
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -313,9 +345,13 @@ int main()
         //model = glm::scale(model, glm::vec3(0.5f, 0.2f, 3.0f)); // transformação de escala não linear
         lightingShader.setMat4("model", model);
 
-        // render the cube
-        glBindVertexArray(objVAO);
-        glDrawArrays(GL_TRIANGLES, 0, vectorSize/11);
+        for (int i = 0; i < modelscount; i++)
+        {
+            glBindTexture(GL_TEXTURE_2D, objects[i].texture);
+            // render the cube
+            glBindVertexArray(objects[i].VAO);
+            glDrawArrays(GL_TRIANGLES, 0, objects[i].pointsCount);
+        }
 
         // also draw the lamp object
         lightCubeShader.use();
@@ -329,7 +365,6 @@ int main()
         glBindVertexArray(lightCubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
@@ -337,11 +372,17 @@ int main()
     }
 
     // optional: de-allocate all resources once they've outlived their purpose:
+
+        for (int i = 0; i < modelscount; i++)
+        {
+            glDeleteVertexArrays(1, &objects[i].VAO);
+            glDeleteBuffers(1, &objects[i].VBO);
+        }
     // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &objVAO);
-    glDeleteVertexArrays(1, &lightCubeVAO);
-    glDeleteBuffers(1, &lightCubeVBO);
-    glDeleteBuffers(1, &VBO);
+
+     glDeleteVertexArrays(1, &lightCubeVAO);
+     glDeleteBuffers(1, &lightCubeVBO);
+
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -364,34 +405,34 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+    {
         specularStrength += 0.01f;
-        if(specularStrength > 5.0f)
+        if (specularStrength > 5.0f)
             specularStrength = 5.0f;
         std::cout << "Especular = " << specularStrength << std::endl;
     }
-    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+    {
         specularStrength -= 0.01f;
-        if(specularStrength < 0.0f)
+        if (specularStrength < 0.0f)
             specularStrength = 0.0f;
         std::cout << "Especular = " << specularStrength << std::endl;
     }
-
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
 
-
 // glfw: whenever the mouse moves, this callback is called
 // -------------------------------------------------------
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+void mouse_callback(GLFWwindow *window, double xpos, double ypos)
 {
     if (firstMouse)
     {
@@ -411,8 +452,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(yoffset);
 }
-
